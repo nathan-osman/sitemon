ca7821c2542525ebe2898e53c1b11289278f4559
Nathan Osman <nathan@quickmediasolutions.com>
2024-09-27 16:25:56 -0700

    Add Spinner component.

---
+++ ui/src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+export default function Spinner() {
+  return (
+    <div className="grow h-full flex justify-center items-center">
+      <div className="text-4xl">
+        Loading...
+      </div>
+    </div>
+  )
+}
